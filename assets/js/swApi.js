64f715c558b2129158eec16f761436622dee6e82
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-02 15:33:27 -0500

    Add basic info of characters

--- assets/js/swApi.js
+++ assets/js/swApi.js
@@ -6,39 +6,62 @@ const getData = (url) => {
   return fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      llenarDatos(json.results), paginacion(json.info);
+      pokeData(json.results), paginacion(json.next, json.previous);
     })
     .catch((error) => {
       console.log("Error: " + error);
     });
 };
 
-// Dibujar cards de personajes
-const llenarDatos = (data) => {
+// const pokedata
+const pokeData = (data) => {
   let html = "";
-  data.forEach((pj) => {
-    html += '<div class="col mt-5">';
-    html += '<div class="card" style="width: 15rem;">';
-    html += `<img src="${pj.image}" class="card-img-top" alt="${pj.name}">`;
-    html += '<div class="card-body">';
-    html += `<h5 class="card-title">${pj.name}</h5>`;
-    html += `<p class="card-text">Status: ${pj.status}</p>`;
-    html += `<p class="card-text">Specie: ${pj.species}</p>`;
-    html += "</div>";
-    html += "</div>";
-    html += "</div>";
+  document.getElementById("datosPersonajes").innerHTML = "";
+  data.forEach((pk) => {
+    let name = pk.name;
+    let height = pk.height;
+    let mass = pk.mass;
+    let gender = pk.gender;
+    const URL = pk.homeworld;
+    return fetch(URL)
+      .then((response) => response.json())
+      .then((json) => {
+        llenarDatos(json, html, name, height, mass, gender);
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
   });
-  document.getElementById("datosPersonajes").innerHTML = html;
+};
+
+// Dibujar cards de Pokemons
+const llenarDatos = (data, html, name, height, mass, gender) => {
+  html += '<div class="col mt-5">';
+  html += '<div class="card" style="width: 15rem;">';
+  html += '<div class="card-body">';
+  html += `<h5 class="card-title">Nombre: ${name}</h5>`;
+  html += `<h5 class="card-title">Altura: ${height}</h5>`;
+  html += `<h5 class="card-title">Masa: ${mass}</h5>`;
+  html += `<h5 class="card-title">Genero: ${gender}</h5>`;
+  html += `<h5 class="card-title">Planeta Origen: ${data.name}</h5>`;
+  html += "</div>";
+  html += "</div>";
+  html += "</div>";
+  document.getElementById("datosPersonajes").innerHTML += html;
 };
 
 // Paginacion
-const paginacion = (data) => {
-    let prevDisabled = "";
-    let nextDisabled = "";
+const paginacion = (next, previous) => {
+  let prevDisabled = "";
+  let nextDisabled = "";
 
-    let html = `<li class="page-item ${data.prev == null ? prevDisabled = "disabled" : prevDisabled = "" }"><a class="page-link" onclick="getData('${data.prev}')">Previous</a></li> <li class="page-item ${data.next == null ? nextDisabled = "disabled" : nextDisabled = "" }"><a class="page-link" onclick="getData('${data.next}')">Next</a></li>`;
+  let html = `<li class="page-item ${
+    previous == null ? (prevDisabled = "disabled") : (prevDisabled = "")
+  }"><a class="page-link" onclick="getData('${previous}')">Previous</a></li> <li class="page-item ${
+    next == null ? (nextDisabled = "disabled") : (nextDisabled = "")
+  }"><a class="page-link" onclick="getData('${next}')">Next</a></li>`;
 
-    document.getElementById("paginacion").innerHTML = html;
+  document.getElementById("paginacion").innerHTML = html;
 };
 
 // Se ejecuta la API
